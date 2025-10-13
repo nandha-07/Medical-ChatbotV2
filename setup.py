1875fda5f249ef566a6efc65887e231791007ac3
Nandha Kumar K <nandhakumar.k7071@gmail.com>
2025-10-14 00:50:01 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Nandy",
+    author_email="nandhakumar.k7071@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
